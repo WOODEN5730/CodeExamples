0c2e68726b38b4d7722ca8084512a006e1c619dd
WOODEN5730 <wooden5730@msn.com>
2023-05-18 08:59:50 -0400

    Added HomeViewModel

---
+++ SwEngHomework/SwEngHomework.WebApp/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SwEngHomework.WebApp.Models
+{
+    public class HomeViewModel
+    {
+        public string CurrentDate =
+    DateTime.UtcNow.ToString("s",System.Globalization.CultureInfo.InvariantCulture); 
+    }
+}
